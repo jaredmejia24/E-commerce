f4d959ee4fd66ec14c47445b6bb26842d56ccce1
jdgc14 <joseojedapro@gmail.com>
2022-06-11 14:52:09 -0500

    generator cards implemented

--- js/script.js
+++ js/script.js
@@ -5,39 +5,39 @@ import { products } from "./data.js";
 console.log(products);
 
 function generateCardProducts(arrayData) {
-    container = document.getElementById(cards-products-container)
-    html = ''
-
+    const container = document.getElementById('cards-products-container');
+    let html = '';
     for(let i = 0; i < arrayData.length; i++) {
         html += `
                 <div class="col-4">
                     <div class="border p-3">
                         <div class="d-flex justify-content-end">
                             <a href="#">
-                                <img src="assets/heart 2.svg" class="icon-1rem" alt="">
+                                <img src="assets/heart 1.svg" class="icon-1rem" alt="">
                              </a>
                         </div>
                         <div class="d-flex justify-content-center">
-                            <img src="assets/image 1.jpg" alt="">
+                            <img src="${arrayData[i].image}" alt="">
                         </div>
                     </div>
                     <div class="border p-3">
                         <div>
-                            <p class="m-0 fs-body2 light-gray-color fw-semibold">SAMSUNG</p>
-                            <p class="fs-subtittle1 dark-color">Galaxy s52</p>
+                            <p class="m-0 fs-body2 light-gray-color fw-semibold">${arrayData[i].brand.toUpperCase()}</p>
+                            <p class="fs-subtittle1 dark-color">${arrayData[i].model}</p>
                         </div>
                         <div class="d-flex justify-content-between align-items-center">
                             <div>
                                 <p class="m-0 fs-body2 light-gray-color fw-semibold">Precio:</p>
-                                <p class="ms-3 fs-subtittle1 dark-color">$5,200.00</p>
+                                <p class="ms-3 fs-subtittle1 dark-color">${arrayData[i].price}</p>
                             </div>
                             <a href="#" class="bg-rose rounded-circle icon-2-5rem d-flex justify-content-center align-items-center">
                                 <img src="assets/shopping-cart 3.svg" class="icon-1-5rem" alt="">
                             </a>
                         </div>
                     </div>
-                </div>
-                `
+                </div>`;
     }
+    container.innerHTML = html;
 }
 
+generateCardProducts(products)
